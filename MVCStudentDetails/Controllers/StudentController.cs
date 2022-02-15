88b7b94d3b91570c05d8e337b1f258a682af835b
Onkar Sharma <osharma@India.epayoffice.net>
2022-02-15 18:29:01 +0530

    CRUD-Operation-In-ASP.NET-MVC

---
+++ MVCStudentDetails/Controllers/StudentController.cs
@@ -0,0 +1,79 @@
+﻿using MVCStudentDetails.Context;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStudentDetails.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        StudentDetailsEntities1 dbobj = new StudentDetailsEntities1();
+        public ActionResult Student(tbl_MVCStudent obj)
+        {
+            return View(obj);
+            //if (obj != null)
+            //{
+            //    return View(obj);
+            //}
+            //else
+            //{
+            //    return View();
+            //}
+        }
+        [HttpPost]
+        public ActionResult AddStudent(tbl_MVCStudent model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    tbl_MVCStudent obj = new tbl_MVCStudent();
+                    obj.ID = model.ID;
+                    obj.Fname = model.Fname;
+                    obj.Lname = model.Lname;
+                    obj.Email = model.Email;
+                    obj.Mobile = model.Mobile;
+                    obj.Description = model.Description;
+
+                    if (model.ID == 0) /*for Insert*/
+                    {
+                        dbobj.tbl_MVCStudent.Add(obj);
+                        dbobj.SaveChanges();
+                    }
+                    else
+                    {
+                        dbobj.Entry(obj).State = EntityState.Modified;
+                        dbobj.SaveChanges();
+                    }
+                }
+                ModelState.Clear();
+                return View("Student");
+            }
+            catch (Exception ex)
+            {
+                return View("Student");
+            }
+        }
+
+        public ActionResult StudentList()
+        {
+            var res = dbobj.tbl_MVCStudent.ToList();
+            return View(res);
+        }
+
+        public ActionResult Delete(int ID)
+        {
+            var res = dbobj.tbl_MVCStudent.Where(x => x.ID == ID).First();
+            dbobj.tbl_MVCStudent.Remove(res);
+            dbobj.SaveChanges();
+
+            var list = dbobj.tbl_MVCStudent.ToList();
+
+            return View("StudentList", list);
+        }
+    }
+}
---
+++ MVCStudentDetails/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCStudentDetails.MvcApplication" Language="C#" %>
